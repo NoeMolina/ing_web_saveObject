8bd1aae0975fbc7ff503bf3e589b29843c1d2869
NoeMolina <l20170752@culiacan.tecnm.mx>
2025-02-06 13:14:16 -0700

    se agregaron las clases auto y motor

---
+++ ing_web-PersistenciaObjetos/README.txt
@@ -0,0 +1,13 @@
+Clases necesarias para el proyecto
+
+-----objetos con agregacion por composición
+clase auto
+la clase auto contiene a la clase motor
+
+
+---objetos con agregacion por referencia
+
+clase termo tiene la clase termometro, si la clase termo se destruye,
+la clase termometro no se destruye.
+
+
---
+++ ing_web-PersistenciaObjetos/src/ing_web/persistenciaobjetos/Auto.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ing_web.persistenciaobjetos;
+
+/**
+ *
+ * @author RICARDO VG LAPTOPS
+ */
+public class Auto {
+    private Motor motor;
+    private float gasolina;
+    private boolean encendido;
+    
+    public Auto(){
+        
+    }
+}
